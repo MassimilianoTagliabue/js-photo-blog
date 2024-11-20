0cf049ed1a99e591ad7ca8926d128e4b9cc03748
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2024-11-20 15:53:47 +0100

    feat:comparsa scomparsa overlay

--- js/function.js
+++ js/function.js
@@ -1,29 +1,53 @@
 
-const generaCard = (text) =>{
+const takeAllCardElem = () => {
+    const cardsElem = document.querySelectorAll(".card-ms");
 
-    axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6",{timeout:5000}).then((resp) =>{
-    
-        
-        let result = resp.data;
-    
-        
-        result.forEach( curItem => {
-    
-            console.log(curItem);
+    cardsElem.forEach((curItem) => {
+
+        curItem.addEventListener("click", ()=>{
+            overlayElem.classList.remove("d-none");
             
-            text += `<div class="card-ms col-4 mt-5">
+
+        })
+        console.log(curItem);
+
+    });
+
+
+
+}
+
+const generaCard = (text) => {
+
+    axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6", { timeout: 5000 }).then((resp) => {
+
+        let result = resp.data;
+
+        result.forEach(curItem => {
+
+            console.log(curItem);
+
+            text += `<div class="card-ms col-4 mt-5" data-id="${curItem.id}">
                         <img src="./img/pin.svg" alt="" class="pin-ms">
                         <div class="square-ms">
                             <img src="${curItem.thumbnailUrl}" alt="" width="300px" heigth="300px">
                         </div>
                         <p class="card-text-ms">${curItem.title} </p>
                     </div>`;
-    
-                  
+
+
         })
-         
+
         cardElem.innerHTML = text;
-        
-        
+        takeAllCardElem();
+
+
+
+
     })
-    }
+
+
+
+
+};
+
--- js/script.js
+++ js/script.js
@@ -2,10 +2,9 @@
 const cardElem = document.getElementById("card");
 const btnOverlay = document.querySelector(".btn-close-ms");
 const overlayElem = document.querySelector(".overlay-ms");
-console.log(overlayElem.classList);
 
-console.log(btnOverlay);
-//overlayElem.classList.add("d-none");
+
+
 
 let text = "";
 
@@ -14,7 +13,8 @@ generaCard(text);
 
 
 
+
 btnOverlay.addEventListener("click", () =>{
 
     overlayElem.classList.add("d-none");
-} )
+} );
